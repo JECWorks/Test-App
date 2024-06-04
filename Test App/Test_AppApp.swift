2ab482ba572a6d6e98483b86cf0ad3ff5bc2e9de
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-06-04 11:03:01 -0400

    Initial Commit

---
+++ Test App/Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppApp.swift
+//  Test App
+//
+//  Created by Jason Cox on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
