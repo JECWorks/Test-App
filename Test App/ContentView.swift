237c4bd7afae5c9f8f02bd2c9e1737eb92b373f6
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-06-05 21:41:44 -0400

    Testing SwiftUI on MacOS

--- Test App/ContentView.swift
+++ Test App/ContentView.swift
@@ -7,15 +7,22 @@
 
 import SwiftUI
 
+var ambuScore = 0
+
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Here's a button")
+            Text("Palliative Performance Scale")
             
-
+            Menu("Ambulation") {
+                Button("Full ambulation", action: fullAmbu)
+                Button("Reduced ambulation", action: reduAmbu)
+                Button("Very reduced ambulation", action: veryAmbu)
+                Button("Bedbound", action: bedBound)
+            }.fixedSize()
             
             Button(action: {
               // Handle sign-in action here
@@ -30,15 +37,32 @@ struct ContentView: View {
                 print("Button tapped.")
             }, label: {
                 /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
-            }).contextMenu(ContextMenu(menuItems: {
-                /*@START_MENU_TOKEN@*/Text("Menu Item 1")/*@END_MENU_TOKEN@*/
-                /*@START_MENU_TOKEN@*/Text("Menu Item 2")/*@END_MENU_TOKEN@*/
-                /*@START_MENU_TOKEN@*/Text("Menu Item 3")/*@END_MENU_TOKEN@*/
-            }))
+            })
             
+
 
         }
         .padding()
+    }
+    func fullAmbu() {
+        print("Full ambulation")
+        ambuScore = 1
+        }
+    func reduAmbu() {
+        print("Reduced ambulation")
+        ambuScore = 2
+        }
+    func veryAmbu() {
+        print("Very reduced ambulation or partially bedbound")
+        ambuScore = 3
+        }
+    func bedBound() {
+        print("Bed bound")
+        ambuScore = 4
+    }
+    
+    func ppsCalc() {
+        
     }
 }
 
